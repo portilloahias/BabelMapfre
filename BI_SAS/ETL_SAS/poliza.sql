ac70ed7497a5fb63a6a063dfee21793f010a15a2
unknown <Capacitaciones@LARISA.grupoq.corp>
2014-03-31 10:08:36 -0600

    add

--- BI_SAS/ETL_SAS/poliza.sql
+++ BI_SAS/ETL_SAS/poliza.sql
@@ -2,81 +2,81 @@
 
 
 
-SELECT    *
+SELECT [CodigoCia]=pol.[cdgocia]
+      ,[DescripcionRamo]=ramo.[nombramo]
+      ,[Certificado]=pol.[numecert]    
+	  ,[CodigoPoliza]=pol.[id]   
+      ,[Poliza]=pol.[numepoli]
+      ,[EstadoPoliza]=ESTADOPOL.desccodi
+	  ,[DescripcionTipoPoliza]=ISNULL(tipopol.desccodi,'NO APLICA')
+      ,[DescripcionFormaDePago]=ISNULL(FormPago.desccodi,'NO APLICA')
+      ,[DescripcionMotivoCancelacion] =ISNULL(BajaPol.desccodi, 'NO APLICA')
+      ,[DescripcionTipoCuenta] =ISNULL(TipoCTA.desccodi,'NO APLICA')
+      ,[DescripcionTipoTarjeta]= CASE LEFT (numecta,1)  
+	                                    WHEN '3' THEN 'AMERICAN EXPRESS'
+										WHEN '4' THEN 'VISA'
+										WHEN '5' THEN 'MASTER CARD'
+										WHEN '6' THEN 'DINERS CLUB'
+										ELSE 'OTRAS' END
+	  ,[IdSistema]=2
 FROM [dbo].[master_pol] as pol
-INNER JOIN [dbo].[master_cod] as codTip
-	ON pol.[id_tipopol]=codTip.id 
-	AND codTip.sinonimo = 'TIPOPOL'
-	AND codTip.desccodi='Individual'
-LEFT JOIN [dbo].[master_cod] as codForPa
-	ON pol.[id_formpago]=codTip.id 
-	AND codTip.sinonimo = 'FORMPAGO'
-LEFT JOIN [dbo].[master_cod] as codCTA
-	ON pol.[id_tipocta]=codCTA.id 
-	AND codCTA.sinonimo = 'TIPOCTA'
-
-
-
-
-SELECT *
-FROM  [dbo].[master_cod]
-where sinonimo like '%TIPOCTA%'
-
-select *
-from [dbo].[master_pol]
-where numecta is not null
-
-
-select *
-FROM  [dbo].[master_cod]
-where sinonimo like '%pag%'
-
-
- 
-
-SELECT   distinct [numecert]
-         ,[parentid]
-FROM [dbo].[master_pol]
-WHERE [parentid] is  not null
-order by [parentid] desc
-
-
-SELECT [id]
-      ,[cdgocia]
-      ,[cdgoramo]
-      ,[numepoli]
-      ,[id_tipopol]
-      ,[numecert]
-      ,[parentid]
-      ,[terceroid]
-      ,[grupo_id]
-      ,[id_estado]
-      ,[fechinsc]
-      ,[fechorig]
-      ,[fechbaja]
-      ,[id_causbaja]
-      ,[fechinic]
-      ,[fechvenc]
-      ,[fechreno]
-      ,[planid]
-      ,[cdgomone]
-      ,[sumaaseg]
-      ,[sumafact]
-      ,[id_formpago]
-      ,[prmaaseg]
-      ,[id_tipotasa]
-      ,[tasaaseg]
-      ,[id_tipocta]
-      ,[numecta]
-      ,[fechvctocta]
-      ,[titularcta]
-      ,[initialid]
-      ,[cdgosucu]
-      ,[ejecutivo]
-      ,[fechultm]
-      ,[userultm]
-      ,[cdgoprod]
-  FROM [dbo].[master_pol]
-GO
-
+INNER JOIN [dbo].[master_cod] as tipopol
+	ON pol.[id_tipopol]=tipopol.id 
+	AND tipopol.sinonimo = 'TIPOPOL'
+	AND tipopol.desccodi='Individual'
+LEFT JOIN [dbo].[master_cod] as EstadoPol
+	ON pol.[id_estado]=EstadoPol.id
+	AND EstadoPol.sinonimo = 'ESTADOPOL'
+LEFT JOIN [dbo].[master_cod] as FormPago
+	ON pol.[id_formpago]=FormPago.id 
+	AND FormPago.sinonimo = 'FORMPAGO'
+LEFT JOIN [dbo].[master_cod] as BajaPol
+	ON pol.[id_causbaja]=BajaPol.id 
+	AND BajaPol.sinonimo = 'CAUSBAJAPOL'
+LEFT JOIN [dbo].[master_cod] as TipoCTA
+	ON pol.[id_tipocta]=TipoCTA.id 
+	AND TipoCTA.sinonimo = 'TIPOCTA'
+LEFT JOIN [dbo].[ramo] as ramo
+	ON pol.[cdgoramo]=ramo.[cdgoramo]
+	AND pol.[cdgocia]=ramo.[cdgocia]
+UNION
+SELECT 
+       [CodigoCia]=pol.[cdgocia]
+      ,[DescripcionRamo]=ramo.[nombramo]
+      ,[Certificado]=pol.[numecert]    
+	  ,[CodigoPoliza]=pol.[id]   
+      ,[Poliza]=pol.[numepoli]
+      ,[EstadoPoliza]=ESTADOPOL.desccodi
+	  ,[DescripcionTipoPoliza]=ISNULL(tipopol.desccodi,'NO APLICA')
+      ,[DescripcionFormaDePago]=ISNULL(FormPago.desccodi,'NO APLICA')
+      ,[DescripcionMotivoCancelacion] =ISNULL(BajaPol.desccodi, 'NO APLICA')
+      ,[DescripcionTipoCuenta] =ISNULL(TipoCTA.desccodi,'NO APLICA')
+      ,[DescripcionTipoTarjeta]= CASE LEFT (numecta,1)  
+	                                    WHEN '3' THEN 'AMERICAN EXPRESS'
+										WHEN '4' THEN 'VISA'
+										WHEN '5' THEN 'MASTER CARD'
+										WHEN '6' THEN 'DINERS CLUB'
+										ELSE 'OTRAS' END
+	  ,[IdSistema]=2
+FROM [dbo].[master_pol] as pol
+INNER JOIN [dbo].[master_cod] as tipopol
+	ON pol.[id_tipopol]=tipopol.id 
+	AND tipopol.sinonimo = 'TIPOPOL'
+	AND tipopol.desccodi='Colectiva'
+LEFT JOIN [dbo].[master_cod] as EstadoPol
+	ON pol.[id_estado]=EstadoPol.id
+	AND EstadoPol.sinonimo = 'ESTADOPOL'
+LEFT JOIN [dbo].[master_cod] as FormPago
+	ON pol.[id_formpago]=FormPago.id 
+	AND FormPago.sinonimo = 'FORMPAGO'
+LEFT JOIN [dbo].[master_cod] as BajaPol
+	ON pol.[id_causbaja]=BajaPol.id 
+	AND BajaPol.sinonimo = 'CAUSBAJAPOL'
+LEFT JOIN [dbo].[master_cod] as TipoCTA
+	ON pol.[id_tipocta]=TipoCTA.id 
+	AND TipoCTA.sinonimo = 'TIPOCTA'
+LEFT JOIN [dbo].[ramo] as ramo
+	ON pol.[cdgoramo]=ramo.[cdgoramo]
+	AND pol.[cdgocia]=ramo.[cdgocia]
+WHERE pol.[numecert] <> '0' AND pol.[numecert] is not null
 
